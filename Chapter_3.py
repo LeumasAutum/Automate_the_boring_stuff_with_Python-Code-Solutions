992a3159e32d6f44d480c37fdcbd2531a569c727
LeumasAutum <mutuasammy76@gmail.com>
2023-02-10 17:40:29 +0300

    Chapter  3, part a practice projects code

--- Chapter_3.py
+++ Chapter_3.py
@@ -39,20 +39,20 @@ while number != 1:
 # user types in a noninteger string. Normally, the int() function will raise a 
 # ValueError error if it is passed a noninteger string, as in int('puppy'). In the 
 # except clause, print a message to the user saying they must enter an integer
-def collatz(number):
-    if number % 2 == 0:
-        result = number // 2
-    else:
-        result = 3 * number + 1
-    print(result)
-    return result
+# def collatz(number):
+#     if number % 2 == 0:
+#         result = number // 2
+#     else:
+#         result = 3 * number + 1
+#     print(result)
+#     return result
 
-while True:
-    try:
-        number = int(input("Enter number: "))
-        break
-    except ValueError:
-        print("You must enter an integer.")
+# while True:
+#     try:
+#         number = int(input("Enter number: "))
+#         break
+#     except ValueError:
+#         print("You must enter an integer.")
 
-while number != 1:
-    number = collatz(number)
+# while number != 1:
+#     number = collatz(number)
